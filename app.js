c6d38f45b1054b428bb508106258b78aca0c0e51
itedumail20 <itedumail20@gmail.com>
2024-03-09 00:21:09 +0500

    main

---
+++ app.js
@@ -0,0 +1,6 @@
+var num = prompt("enter reguired table number");
+for(i=1;i<=10;i++){
+    document.write("<div>")
+    document.write(num + " X " + i + " = " + i*num);
+    document.write("</div>")
+}
